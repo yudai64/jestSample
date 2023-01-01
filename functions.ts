2c6f9c333fe606c106badb166f92e9e9d6aebe2f
Yudai Yano <yudaiyano@macbookair.local>
2023-01-01 14:40:15 +0900

    課題4を追加

--- functions.ts
+++ functions.ts
@@ -40,3 +40,33 @@ export const getFirstNameThrowIfLong = async (
   }
   return firstName;
 };
+
+// 課題4
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+export const getTodo = async (id: number): Promise<Todo> => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  return await response.json();
+};
+
+export const getTodos = async (): Promise<Todo[]> => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+  return await response.json();
+};
+
+export const createTodo = async (todo: Todo): Promise<number> => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+  });
+  const data = await response.json();
+  return data.id;
+};
